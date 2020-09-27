e868e9942dd0978eedd657c67f6071e13687b855
junhwong <junhwong@users.noreply.github.com>
2020-09-27 17:03:34 +0800

    cache: 增加基本实现

--- cache/cache.go
+++ cache/cache.go
@@ -5,21 +5,30 @@ import (
 	"time"
 )
 
-type Loader func() (value interface{}, err error)
+// IndexFunc knows how to compute the set of indexed values for an object.
+type IndexFunc func(obj interface{}) ([]string, error)
+
+//
+type Loader func() (value interface{}, expiration time.Duration, err error)
 
 type Filter func(key string, entry CacheEntry) bool
 
 type Cache interface {
+	Set(key string, value interface{}, expiration time.Duration) (present interface{}, err error)
 	// Returns the value associated with key in this cache, obtaining that value from loader if necessary.
 	Get(key string, loader Loader) (value interface{}, err error)
-	// Returns the value associated with key in this cache, or null if there is no cached value for key.
+	// Returns the value associated with key in this cache, or nil if there is no cached value for key.
 	GetIfPresent(key string) (value interface{})
-	// Discards any cached value for key key.
-	Invalidate(filter Filter)
+	GetIfPresentE(key string) (value interface{}, err error)
+	List(filter Filter) error
+	DeleteE(key string) (value interface{}, err error)
+	Delete(key string) interface{}
 }
 
 type CacheEntry interface {
-	Expiration() time.Duration
+	// Key() string
+	Value() interface{}
+	IsExpired() bool
 }
 
 func FilterWithKey(key string) Filter {
@@ -27,3 +36,16 @@ func FilterWithKey(key string) Filter {
 		return strings.EqualFold(key, k)
 	}
 }
+
+type Empty struct{}
+
+type String map[string]Empty
+
+// Index maps the indexed value to a set of keys in the store that match on that value
+type Index map[string]String
+
+// Indexers maps a name to a IndexFunc
+type Indexers map[string]IndexFunc
+
+// Indices maps a name to an Index
+type Indices map[string]Index
--- cache/mem.go
+++ cache/mem.go
@@ -1,4 +1,147 @@
 package cache
 
-type MemCache struct {
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+type entry struct {
+	// key        string
+	expiration time.Duration
+	value      interface{}
+}
+
+// func (e *entry) Key() string {
+// 	if e == nil {
+// 		return ""
+// 	}
+// 	return e.key
+// }
+func (e *entry) Value() interface{} {
+	if e == nil {
+		return nil
+	}
+	return e.value
+}
+func (e *entry) IsExpired() bool {
+	if e == nil {
+		return true
+	}
+	if e.expiration <= 0 {
+		return false
+	}
+	t := time.Now()
+	return t.After(t.Add(e.expiration))
+}
+
+type threadSafeMap struct {
+	lock  sync.RWMutex
+	items map[string]*entry
+}
+
+func NewMemCache() Cache {
+	return &threadSafeMap{
+		items: map[string]*entry{},
+	}
+}
+
+func (c *threadSafeMap) update(key string, value interface{}, expiration time.Duration) (interface{}, error) {
+	if value == nil {
+		return c.delete(key)
+	}
+	item := c.items[key]
+	if item == nil {
+		item = &entry{} //key: key
+		c.items[key] = item
+	}
+
+	item.expiration = expiration
+	item.value = value
+	return item.value, nil
+}
+
+func (c *threadSafeMap) delete(key string) (interface{}, error) {
+	item := c.items[key]
+	delete(c.items, key)
+	if item == nil {
+		return nil, nil
+	}
+	return item.value, nil
+}
+
+func (c *threadSafeMap) Set(key string, value interface{}, expiration time.Duration) (interface{}, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	return c.update(key, value, expiration)
+}
+func (c *threadSafeMap) get(key string) (*entry, error) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+
+	return c.items[key], nil
+}
+
+func (c *threadSafeMap) Get(key string, loader Loader) (value interface{}, err error) {
+	item, err := c.get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if item.IsExpired() {
+		if loader == nil {
+			return nil, errors.New("loader cannt be nil")
+		}
+		value, expiration, err := loader()
+		if err != nil || value == nil {
+			return nil, err
+		}
+		return c.Set(key, value, expiration)
+	}
+	return item.value, nil
+}
+
+func (c *threadSafeMap) GetIfPresentE(key string) (value interface{}, err error) {
+	item, err := c.get(key)
+	if err != nil || item == nil {
+		return nil, err
+	}
+	if item.IsExpired() {
+		return c.DeleteE(key)
+	}
+	return item.value, nil
+}
+
+func (c *threadSafeMap) GetIfPresent(key string) (value interface{}) {
+	value, _ = c.GetIfPresentE(key)
+	return
+}
+
+func (c *threadSafeMap) DeleteE(key string) (value interface{}, err error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	return c.delete(key)
+}
+
+func (c *threadSafeMap) Delete(key string) (value interface{}) {
+	value, _ = c.DeleteE(key)
+	return
+}
+
+func (c *threadSafeMap) List(filter Filter) error {
+	// TODO: 效率
+	c.lock.RLock()
+	copied := make(map[string]*entry, len(c.items))
+	for k, v := range c.items {
+		copied[k] = v
+	}
+	c.lock.RUnlock()
+	for k, v := range copied {
+		if stop := filter(k, v); stop {
+			return nil
+		}
+	}
+	return nil
 }
