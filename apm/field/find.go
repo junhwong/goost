cadd526405a97de3d0d0fa335210ea7281451ec9
junhwong <junhwong@users.noreply.github.com>
2024-09-16 12:20:46 +0800

    增加初始化参数

--- apm/field/find.go
+++ apm/field/find.go
@@ -19,6 +19,15 @@ func FindWith(p jsonpath.Expr, root *Field) ([]*Field, error) {
 	return v.current, v.Error()
 }
 
+func FindWithCurrent(p jsonpath.Expr, root *Field, current []*Field) ([]*Field, error) {
+	v := &explorer{readonly: true, root: root, current: current, parent: []*Field{}}
+	v.Visit = func(e jsonpath.Expr) {
+		jsonpath.Visit(e, v, v.SetError)
+	}
+	v.Visit(p)
+	return v.current, v.Error()
+}
+
 func ParseNamePath(nameOrPath string) (jsonpath.Expr, error) {
 	return jsonpath.Parse(nameOrPath)
 }
