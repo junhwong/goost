d1064d669192439ee402829f9ea11efb82c4a911
junhwong <junhwong@users.noreply.github.com>
2024-10-06 21:14:45 +0800

    增加持续时间的解析

--- apm/field/convert.go
+++ apm/field/convert.go
@@ -3,6 +3,7 @@ package field
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/spf13/cast"
@@ -172,7 +173,22 @@ func As(f *Field, t Type, layouts []string, loc *time.Location) error {
 			panic("todo convert")
 		}
 	case DurationKind:
-		panic("todo convert:DurationKind")
+		switch f.GetType() {
+		case StringKind:
+			s := f.GetString()
+			d, err := ParseDuration(s)
+			if err == nil {
+				d, err = ParseMomentDuration(s)
+			}
+			if err != nil {
+				return fmt.Errorf("invalid duration: %s", s)
+			}
+			f.SetDuration(d)
+			return err
+		default:
+			panic("todo convert:DurationKind")
+		}
+
 	case GroupKind:
 
 		switch {
@@ -280,4 +296,194 @@ func ToRowTable(dest *Field, cols []*Field) {
 		rows = append(rows, row)
 	}
 	dest.SetArray(rows, false)
+}
+
+// 解析相对于基础时间，返回时间间隔
+func ParseMomentDuration(s string) (time.Duration, error) {
+	base := time.Now()
+	s = strings.ReplaceAll(s, "&nbsp;", " ")
+	s = strings.TrimSpace(s)
+	switch s {
+	case "":
+		return 0, nil
+	case "刚刚", "1秒前", "一秒前", "just", "just now", " last second", "1 second ago":
+		return base.Sub(base.Add(-1 * time.Second)), nil
+	case "1分钟前", "1 min ago", "1 minute ago", "last minute", "一分钟前":
+		return base.Sub(base.Add(-1 * time.Minute)), nil
+	case "1小时前", "1 hour ago", "last hour", "一小时前", "last hour ago":
+		return base.Sub(base.Add(-1 * time.Hour)), nil
+	case "昨天", "yesterday", "1 day ago", "一天前", "1天前", "last day":
+		return base.Sub(base.AddDate(0, 0, -1)), nil
+	case "前天", "the day before yesterday":
+		return base.Sub(base.AddDate(0, 0, -2)), nil
+	case "一周前", "a week ago", "1周前", "1 week ago", "last week":
+		return base.Sub(base.AddDate(0, 0, -7)), nil
+	case "一个月前", "a month ago", "1个月前", "1 month ago", "last month":
+		return base.Sub(base.AddDate(0, -1, 0)), nil
+	case "一年前", "a year ago", "1年前", "1 year ago", "last year":
+		return base.Sub(base.AddDate(-1, 0, 0)), nil
+	}
+
+	if s, ok := strings.CutSuffix(s, "years ago"); ok {
+		n, err := cast.ToIntE(s)
+		if err != nil {
+			return 0, err
+		}
+		return base.Sub(base.AddDate(-1*n, 0, 0)), nil
+	}
+	if s, ok := strings.CutSuffix(s, "年前"); ok {
+		n, err := cast.ToIntE(s)
+		if err != nil {
+			return 0, err
+		}
+		return base.Sub(base.AddDate(-1*n, 0, 0)), nil
+	}
+	if s, ok := strings.CutSuffix(s, "months ago"); ok {
+		n, err := cast.ToIntE(s)
+		if err != nil {
+			return 0, err
+		}
+		return base.Sub(base.AddDate(0, -1*n, 0)), nil
+	}
+	if s, ok := strings.CutSuffix(s, "个月前"); ok {
+		n, err := cast.ToIntE(s)
+		if err != nil {
+			return 0, err
+		}
+		return base.Sub(base.AddDate(0, -1*n, 0)), nil
+	}
+	if s, ok := strings.CutSuffix(s, "days ago"); ok {
+		n, err := cast.ToIntE(s)
+		if err != nil {
+			return 0, err
+		}
+		return base.Sub(base.AddDate(0, 0, -1*n)), nil
+	}
+	if s, ok := strings.CutSuffix(s, "天前"); ok {
+		n, err := cast.ToIntE(s)
+		if err != nil {
+			return 0, err
+		}
+		return base.Sub(base.AddDate(0, 0, -1*n)), nil
+	}
+
+	if s, ok := strings.CutSuffix(s, "ago"); ok {
+		return ParseDuration(s)
+	}
+
+	if s, ok := strings.CutSuffix(s, "前"); ok {
+		return ParseDuration(s)
+	}
+
+	return 0, fmt.Errorf("unsupported format %s", s)
+}
+
+func ParseDuration(os string) (time.Duration, error) {
+	s := strings.ReplaceAll(os, "&nbsp;", " ")
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return 0, nil
+	}
+	if strings.Contains(s, ":") { // 00:00:00
+		a := strings.Split(s, ":")
+		l := len(a)
+		if l < 3 {
+			return 0, fmt.Errorf("unsupported format %s", os)
+		}
+
+		return 0, nil
+	}
+	s = strings.ReplaceAll(s, "小时", "h")
+	s = strings.ReplaceAll(s, "分钟", "m")
+	s = strings.ReplaceAll(s, "秒钟", "s")
+	s = strings.ReplaceAll(s, "天", "d")
+	s = strings.ReplaceAll(s, "时", "h")
+	s = strings.ReplaceAll(s, "分", "m")
+	s = strings.ReplaceAll(s, "秒", "s")
+	s = strings.ReplaceAll(s, "年", "y")
+	s = strings.ReplaceAll(s, "月", "M")
+	s = strings.ReplaceAll(s, "周", "w")
+	s = strings.TrimSpace(s)
+
+	h := 0
+	if arr := strings.SplitN(s, "y", 2); len(arr) == 2 {
+		n, err := cast.ToIntE(arr[0])
+		if err != nil {
+			return 0, err
+		}
+		h += n * 360 * 24
+		s = arr[1]
+	}
+	if arr := strings.SplitN(s, "M", 2); len(arr) == 2 {
+		n, err := cast.ToIntE(arr[0])
+		if err != nil {
+			return 0, err
+		}
+		h += n * 30 * 24
+		s = arr[1]
+	}
+	if arr := strings.SplitN(s, "w", 2); len(arr) == 2 {
+		n, err := cast.ToIntE(arr[0])
+		if err != nil {
+			return 0, err
+		}
+		h += n * 7 * 24
+		s = arr[1]
+	}
+	if arr := strings.SplitN(s, "d", 2); len(arr) == 2 {
+		n, err := cast.ToIntE(arr[0])
+		if err != nil {
+			return 0, err
+		}
+		h += n * 24
+		s = arr[1]
+	}
+	if arr := strings.SplitN(s, "h", 2); len(arr) == 2 {
+		n, err := cast.ToIntE(arr[0])
+		if err != nil {
+			return 0, err
+		}
+		h += n
+		s = arr[1]
+	}
+	// ns:=0
+	// if arr := strings.SplitN(s, "ns", 2); len(arr) == 2 {
+	// 	n, err := cast.ToIntE(arr[1])
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	ns = n
+	// 	s = arr[0]
+	// }
+	// us:=0
+	// if arr := strings.SplitN(s, "us", 2); len(arr) == 2 {
+	// 	n, err := cast.ToIntE(arr[0])
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	us = n
+	// 	s = arr[1]
+	// }
+	m := 0
+	if arr := strings.SplitN(s, "m", 2); len(arr) == 2 {
+		n, err := cast.ToIntE(arr[0])
+		if err != nil {
+			return 0, err
+		}
+		m = n
+		s = arr[1]
+	}
+	sec := 0
+	if arr := strings.SplitN(s, "s", 2); len(arr) != 0 { // todo 小数, 毫秒
+		n, err := cast.ToIntE(arr[0])
+		if err != nil {
+			return 0, err
+		}
+		sec = n
+		s = arr[1]
+	}
+	if s != "" {
+		fmt.Printf("unexpected suffix %q:%q\n", s, os)
+	}
+	return time.Duration(h*3600+m*60+sec) * time.Second, nil
 }
--- apm/field/time_test.go
+++ apm/field/time_test.go
@@ -1,6 +1,7 @@
 package field
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,6 +21,37 @@ func TestReplaceToGoTimeTempl(t *testing.T) {
 		t.Run(tC.layout, func(t *testing.T) {
 			v := replaceToGoTimeTempl(tC.layout)
 			assert.Equal(t, tC.expected, v)
+		})
+	}
+}
+
+func TestParseMoment(t *testing.T) {
+	// 13天22小时
+	// last year
+	// last month
+	// last week
+	// 3 years ago
+	// 2 months ago
+	// 5 days ago
+	// 16 hours ago
+	// yesterday
+	// on Mar 20
+	// on Oct 6, 2023
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "刚刚",
+		},
+		{
+			desc: "last week",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			v, err := ParseMomentDuration(tC.desc)
+			assert.NoError(t, err)
+			fmt.Printf("v: %v\n", v)
 		})
 	}
 }
